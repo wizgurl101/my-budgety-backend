77dfbb2dafcb0aae2c754805e35e7ce860063184
wizgurl101 <nb.phan@hotmail.com>
2024-03-13 22:36:57 -0600

    add swagger ui to project

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,1 +1,9 @@
 import { Request, Response } from "express";
+
+// todo For testing purpose delete later
+export const testUser = (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "success",
+    message: "Test User works",
+  });
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,24 @@
 import express from "express";
+import { testUser } from "../controllers/userController";
 
 const router = express.Router();
+
+// todo For testing purpose delete later
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Get Test User Endpoint
+ *     description: Successfully call this endpoint
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               Status: string
+ *               Message: string
+ */
+router.get("/test", testUser)
 
 export default router;
