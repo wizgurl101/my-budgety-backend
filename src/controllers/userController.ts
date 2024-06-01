d1da96783265a2240fdc6da95aa9555ca221e701
wizgurl101 <nb.phan@hotmail.com>
2024-05-31 22:15:23 -0600

    Add query to get all keywords for a user
    - API is not working in postman

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
-import { executeUserQuery } from "../db/bigquery";
+import { executeUserQuery, executeGetAllKeywords } from "../db/bigquery";
 
 // todo For testing purpose delete later
 export const testUser = (req: Request, res: Response) => {
-  return res.status(200).json({
+    console.log('Test User Called');
+    return res.status(200).json({
     status: "success",
     message: "Test User works",
   });
@@ -27,5 +28,25 @@ export const GetUser = async (req: Request, res: Response) => {
 }
 
 export const GetCurrentMonthSumExpanse = async (req: Request, res: Response) => {
+  //todo finish implementation
+}
 
+export const GetAllKeywords = async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId;
+        console.log('Get All User Keywords')
+        console.log(userId)
+        const keywords = await executeGetAllKeywords(userId);
+        return res.status(200).json({
+          status: "success",
+          message: "Get all keywords successfully fetched",
+          data: keywords,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+          status: "error",
+          message: "Internal Server Error",
+        });
+    }
 }
