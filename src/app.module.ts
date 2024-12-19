42520f036322bbab0226749c5a476256fb19d3f0
wizgurl101 <nb.phan@hotmail.com>
2024-12-19 09:07:46 -0700

    add upload csv module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from "./category/category.module";
 import { KeywordModule} from "./keyword/keyword.module";
 import { ExpanseModule } from "./expanse/expanse.module";
-import { UploadCsvModule } from "./uploadCsv/uploadCsv.module";
 
 @Module({
   imports: [
@@ -13,7 +12,6 @@ import { UploadCsvModule } from "./uploadCsv/uploadCsv.module";
       CategoryModule,
       KeywordModule,
       ExpanseModule,
-      UploadCsvModule,
   ],
 })
 export class AppModule {}
