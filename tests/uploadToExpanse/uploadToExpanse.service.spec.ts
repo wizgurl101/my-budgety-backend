c874855970eff5bd808348cd19f764967f826e42
wizgurl101 <nb.phan@hotmail.com>
2024-12-19 17:35:31 -0700

    Create uploadToExpanse.service.spec.ts

---
+++ tests/uploadToExpanse/uploadToExpanse.service.spec.ts
@@ -0,0 +1,76 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UploadToExpanseService } from '../../src/uploadToExpanse/uploadToExpanse.service';
+import { ConfigService } from '@nestjs/config';
+import { BigQueryService } from '../../src/db/bigQuery/bigquery.service';
+import { FileUtilsService } from '../../src/utils/fileUtils/fileUtils.service';
+import { CategoryService } from '../../src/category/category.service';
+import { KeywordService } from '../../src/keyword/keyword.service';
+import { UuidService } from '../../src/utils/uuid/uuid.service';
+
+describe('UploadToExpanseService', () => {
+  let uploadToExpanseService: UploadToExpanseService;
+  let categoryService: CategoryService;
+  let bigQueryService: BigQueryService;
+  let configService: ConfigService;
+  let uuidService: UuidService;
+  let keywordService: KeywordService;
+  let fileUtilsService: FileUtilsService;
+
+  it('should be defined', async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UploadToExpanseService,
+        {
+          provide: BigQueryService,
+          useValue: {
+            query: jest.fn(),
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+        {
+          provide: UuidService,
+          useValue: {
+            generate: jest.fn(),
+          },
+        },
+        {
+          provide: KeywordService,
+          useValue: {
+            create: jest.fn(),
+            update: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: CategoryService,
+          useValue: {
+            getAllCategoryWithKeywords: jest.fn(),
+          },
+        },
+        {
+          provide: FileUtilsService,
+          useValue: {
+            getDataFromCsv: jest.fn(),
+          },
+        }
+      ],
+    }).compile();
+
+    uploadToExpanseService = module.get<UploadToExpanseService>(
+      UploadToExpanseService,
+    );
+    categoryService = module.get<CategoryService>(CategoryService);
+    bigQueryService = module.get<BigQueryService>(BigQueryService);
+    configService = module.get<ConfigService>(ConfigService);
+    uuidService = module.get<UuidService>(UuidService);
+    keywordService = module.get<KeywordService>(KeywordService);
+    fileUtilsService = module.get<FileUtilsService>(FileUtilsService);
+
+    expect(uploadToExpanseService).toBeDefined();
+  });
+});
