828397cadc3b15e6936594b30e5b111371f32885
wizgurl101 <nb.phan@hotmail.com>
2024-12-24 12:47:23 -0700

    add test if there is a payment in the csv file

--- tests/fileUtils/fileUtils.service.spec.ts
+++ tests/fileUtils/fileUtils.service.spec.ts
@@ -18,7 +18,7 @@ describe('File Utils Service', () => {
     dateUtilsService = module.get<DateUtilsService>(DateUtilsService);
   });
 
-  it('should mock getDataFromCsv method', async () => {
+  it('Get Data From Csv function should return the a list of expanses', async () => {
     const mockCsvData = [
       "12/19/2024,husky,10,,272.61\r\n12/19/2024,shell,25,,272.6\r\n"
     ];
@@ -37,6 +37,24 @@ describe('File Utils Service', () => {
         date: new Date('2024-12-19T07:00:00.000Z'),
         name: 'shell',
         amount: 25,
+        used: false,
+      },
+    ]);
+  });
+
+  it('Given a payment entry in the csv Then Get Data From Csv function should return the a list of one expanses', async () => {
+    const mockCsvData = [
+      "12/19/2024,husky,10,,272.61\r\n12/19/2024,payment,,50,272.6\r\n"
+    ];
+
+    jest.spyOn(fileUtilsService, 'getDataFromCsv').mockResolvedValue(mockCsvData);
+
+    const result = await fileUtilsService.getCsvExpanses('mockFilePath');
+    expect(result).toEqual([
+      {
+        date: new Date('2024-12-19T07:00:00.000Z'),
+        name: 'husky',
+        amount: 10,
         used: false,
       },
     ]);
