5ff67506c328efdd157c3986721a088752b84fa1
wizgurl101 <nb.phan@hotmail.com>
2026-04-15 15:50:00 -0600

    put restrict on tasks columns

--- scripts/mybudgety_dailytasks_scripts.sql
+++ scripts/mybudgety_dailytasks_scripts.sql
@@ -2,8 +2,8 @@ CREATE TABLE dailytasks (
     id uuid,
     user_id uuid,
     date date,
-    task1 integer,
-    task2 integer,
+    task1 integer CHECK (task1 IN (0, 1)),
+    task2 integer CHECK (task2 IN (0, 1))
 );
 
 INSERT INTO dailytasks VALUES (gen_random_uuid(), 'user_id', CURRENT_DATE, 0, 0);
